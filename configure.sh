3ee820b704210777763e24c0980a3a382b2bfdba
Jean-Marie Madiot <madiot@gmail.com>
2020-03-31 12:37:36 +0200

    cleaned build

--- configure.sh
+++ configure.sh
@@ -11,12 +11,9 @@ make -C zoo clean
 make -C zoo sc_nosm.v tso_nosm.v lamport.v x86tso.v
 
 ls */*.v \
-   | grep -v 'DecidablePropKat' \
    | grep -v 'poubelle/' \
    | grep -v '^easy_tso_sc' \
    | grep -v '^testing/' \
-   | grep -v '^lib/dprop_rel.v' \
-   | grep -v '^lib/dprop.v' \
    >> _CoqProject
 
 rm -f */*.vo */*.vok */*.vos */*.glob */.*.aux
